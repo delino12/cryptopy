84f5e813df2795c58df732fa80a4bf6c3dd2ce33
Ekpoto Liberty <ekpoto.liberty@gmail.com>
2018-01-26 19:37:53 +0100

    added blockchain price exchange table

--- _controllers/pages-controller.js
+++ _controllers/pages-controller.js
@@ -1,23 +1,5 @@
-// load blockchain info
-let blockchain = require('blockchain.info');
-let exchange   = require('blockchain.info/exchange');
-
-
 // export the home page controller
 exports.homePage = function (req, res){
 	// json data
-	let data = new Array()
-
-	// initilze block chain
-	exchange.getTicker().then(function (e){
-	    // data.push(e)
-	    console.log(e)
-	}).catch(function (e){
-		var data = {
-			status: 'Error',
-			message: 'Fail to request chain'
-		};
-		console.log(JSON.stringify(data))
-	});
 	res.render('welcome')
 }
--- _routes/web.js
+++ _routes/web.js
@@ -3,8 +3,13 @@ let express = require('express')
 let router = express.Router()
 
 // init controller
-let pages = require('../_controllers/pages-controller')
+let pages      = require('../_controllers/pages-controller')
+let blockchain = require('../_controllers/blockchain-controller'); 
 
+// blockchain API [API ROUTE]
+router.get('/api/tickers', blockchain.tickers);
+
+// get homepage [APP ROUTE]
 router.get('/', pages.homePage)
 
 module.exports = router;
